963de7b01e7de65d3a88744d7d7042d21331f00b
Rois Faozi <roisfaozi55@gmail.com>
2020-07-07 02:06:40 +0700

    Menambahkan code error baru

--- app/src/main/java/com/kotlin/mytestingapp/MainActivity.kt
+++ app/src/main/java/com/kotlin/mytestingapp/MainActivity.kt
@@ -5,22 +5,34 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var btnSetValue: Button
-    private lateinit var tvTeext: TextView
+    private lateinit var tvText: TextView
+
+    private  var names = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tvTeext= findViewById(R.id.tv_text)
+        tvText= findViewById(R.id.tv_text)
         btnSetValue.setOnClickListener(this)
+
+        names.add("Rois Faozi")
+        names.add("kamu Jahat")
+        names.add("Kurang Bagus")
     }
 
-    override fun onClick(v: View) {
+    override fun onClick(view: View) {
         if (view.id == R.id.btn_set_value) {
-            tvTeext.text="19"
+            val name = StringBuilder()
+            for (i in 0..3) {
+                name.append(names[i]).append("\n")
+            }
+            tvText.text="19"
         }
     }
 }
