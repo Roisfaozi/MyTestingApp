70f57b0664bd5b51c925e891af392bf5676878b5
Rois Faozi <roisfaozi55@gmail.com>
2020-07-07 02:25:58 +0700

    Menambahkan code error Gambar OutOfMemoryException

--- app/src/main/java/com/kotlin/mytestingapp/MainActivity.kt
+++ app/src/main/java/com/kotlin/mytestingapp/MainActivity.kt
@@ -2,9 +2,13 @@ package com.kotlin.mytestingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -14,9 +18,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private  var names = ArrayList<String>()
 
+    private lateinit var imagePriview: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSetValue= findViewById(R.id.btn_set_value)
 
         tvText= findViewById(R.id.tv_text)
         btnSetValue.setOnClickListener(this)
@@ -24,12 +32,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         names.add("Rois Faozi")
         names.add("kamu Jahat")
         names.add("Kurang Bagus")
+
+        imagePriview = findViewById(R.id.img_preview)
+        img_preview.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.fronalpstock_big))
     }
 
     override fun onClick(view: View) {
         if (view.id == R.id.btn_set_value) {
             val name = StringBuilder()
-            for (i in 0..3) {
+            for (i in 0..2) {
                 name.append(names[i]).append("\n")
             }
             tvText.text="19"
